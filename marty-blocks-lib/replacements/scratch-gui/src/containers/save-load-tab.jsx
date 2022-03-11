5f4127675d7dce60851ffe96324f1ba8f4f50ceb
robdobsn <rob@dobson.com>
2022-03-11 02:24:02 +0000

    Added save-load files

---
+++ marty-blocks-lib/replacements/scratch-gui/src/containers/save-load-tab.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-warning-comments */
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/jsx-no-literals */
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import SaveLoad from '../components/marty-save-load/save-load.jsx';
+
+const SaveLoadTab = props => <SaveLoad {...props} />;
+
+export default SaveLoadTab;
