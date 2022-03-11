e02421bd77ebbe14d10cfe3cbbb23d484317e1a6
robdobsn <rob@dobson.com>
2022-03-11 13:30:09 +0000

    Removed attempt to take blocks out of scratch-blocks

--- marty-blocks-lib/src/index.js
+++ marty-blocks-lib/src/index.js
@@ -1,4 +1,3 @@
-export {MartyBlocks_BlockLib_AddBlocks} from "./MartyBlocks_BlockLib_AddBlocks.js"
 export {Scratch3Mv2Blocks} from "./Scratch3Mv2Blocks.js"
 export {Mv2Interface} from "./Mv2Interface.js"
 export {MartyBlocksToolbox_motion, 
