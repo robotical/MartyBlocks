c250367ec5a068f9721532e5b30a7c6c406f8f87
nikthegeek1 <ntheodoropoulos@outlook.com>
2023-02-18 19:39:43 +0200

    setting production when building

---
+++ marty-blocks-lib/set-production.js
@@ -0,0 +1,19 @@
+// const fs = require('fs');
+import fs from "fs";
+
+const paths = [
+  "./replacements/scratch-vm/webpack.config.js",
+  "./replacements/scratch-blocks/webpack.config.js",
+  "./replacements/scratch-gui/webpack.config.js",
+];
+
+paths.forEach((path) => {
+  // Read the contents of the file
+  const data = fs.readFileSync(path, "utf-8");
+
+  // Replace the `mode` line with the new value
+  const newData = data.replace("mode: 'development'", "mode: 'production'");
+
+  // Write the new data back to the file
+  fs.writeFileSync(path, newData, "utf-8");
+});
