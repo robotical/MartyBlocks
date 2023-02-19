f173b23a7d13247d4b562247fd1934ad4a80e984
nikthegeek1 <ntheodoropoulos@outlook.com>
2023-02-19 21:58:30 +0200

    trying to resolve the encoding issue

--- marty-blocks-lib/replacements/scratch-blocks/webpack.config.js
+++ marty-blocks-lib/replacements/scratch-blocks/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = [{
     hints: false
   }
 }, {
-  mode: 'production',
+  mode: 'development',
   entry: {
     horizontal: './shim/horizontal.js',
     vertical: './shim/vertical.js'
@@ -49,7 +49,7 @@ module.exports = [{
   plugins: []
 },
 {
-  mode: 'production',
+  mode: 'development',
   entry: './shim/gh-pages.js',
   output: {
     filename: '[name].js',
