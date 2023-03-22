32ba3e9fa3363dfa852aa906783219329dff3eac
nikthegeek1 <ntheodoropoulos@outlook.com>
2023-03-22 07:00:29 +0200

    added error handler for MST and re-addedd project-fetcher which was mysteriously deleted

--- marty-blocks-lib/src/MSTTesting.js
+++ marty-blocks-lib/src/MSTTesting.js
@@ -3,19 +3,28 @@
  * Interface for testing Mart Blocks in MST
  */
 
+window.addEventListener("error", function (event) {
+      mv2Interface.sendFeedbackToServer("MartyBlocks error occurred:", event.error);
+});
+
+window.addEventListener("unhandledrejection", function (event) {
+    mv2Interface.sendFeedbackToServer("MartyBlocks unhandled promise rejection occurred: " + event.reason);
+  console.error();
+});
+
 class MSTTesting {
-    constructor() {
-        this.dance = this.dance.bind(this);
-    }
+  constructor() {
+    this.dance = this.dance.bind(this);
+  }
 
-    dance() {
-        window.vm.runtime._primitives.mv2_dance();
-    }
+  dance() {
+    window.vm.runtime._primitives.mv2_dance();
+  }
 
-    vanillaJS(jsCode) {
-        const results = JSON.stringify(eval(jsCode));
-        mv2Interface.sendFeedbackToServer(results);
-    }
+  vanillaJS(jsCode) {
+    const results = JSON.stringify(eval(jsCode));
+    mv2Interface.sendFeedbackToServer(results);
+  }
 }
 
 module.exports = MSTTesting;
