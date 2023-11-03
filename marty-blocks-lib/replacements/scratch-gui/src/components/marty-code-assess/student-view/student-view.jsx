76c9676b91dbde9e905cc5ef57ee1b6ad2872aae
NikTheGeek1 <ntheodoropoulos@outlook.com>
2023-11-03 21:26:01 +0200

    working on student view

--- marty-blocks-lib/replacements/scratch-gui/src/components/marty-code-assess/student-view/student-view.jsx
+++ marty-blocks-lib/replacements/scratch-gui/src/components/marty-code-assess/student-view/student-view.jsx
@@ -1,63 +1,114 @@
 import React from "react";
-import Modal from "../../../containers/modal.jsx";
-import ScoresCard from "../scores-card/scores-card.jsx";
 import styles from "./student-view.css";
-import DetailsCard from "../details-card/details-card.jsx";
+import bindAll from 'lodash.bindall';
 
 class StudentView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            scores: codeAssess.assess(vm.runtime.targets),
-            showModal: false,
-            modalData: { content: null, title: "" },
+            student: null,
+            studentClassess: [],
+            selectedClass: null,
+            selectedClassStudents: [],
+            selectedClassCourseWorks: [],
+            selectedCourseWork: null,
         };
-        this.closeModal = this.closeModal.bind(this);
-        this.openModal = this.openModal.bind(this);
-        this.totalScore = this.totalScore.bind(this);
-    }
-    
-    closeModal() {
-        this.setState({ showModal: false });
+        bindAll(this, [
+            'handleClassChange',
+            'onAssessMe'
+        ]);
     }
 
-    openModal(modalData) {
-        this.setState({ showModal: true, modalData: modalData });
+    componentDidMount() {
+        // get the student object
+        this.setState({ student: codeAssess.student });
     }
 
-    totalScore() {
-        let total = 0;
-        Object.keys(this.state.scores).forEach(categoryKey => {
-            total += this.state.scores[categoryKey];
-        })
-        return total;
+    // when the component state is changed
+    componentDidUpdate(prevProps, prevState) {
+        // when the selectedClass changes
+        if (prevState.selectedClass !== this.state.selectedClass) {
+            this.getStudentsOfClass();
+            this.getCourseworskOfClass();
+        }
+        // when the student changes
+        if (prevState.student !== this.state.student) {
+            this.state.student.getListOfClassess().then((classess) => {
+                this.setState({ studentClassess: classess });
+            });
+        }
+        // when the selectedCourseWork changes
+        if (prevState.selectedCourseWork !== this.state.selectedCourseWork) {
+            console.log("selectedCourseWork changed", this.state.selectedCourseWork);
+        }
+    }
+
+    handleClassChange(event) {
+        console.log("handleClassChange")
+        this.setState({ selectedClass: this.state.studentClassess[event.target.value] });
+    }
+
+    // when the studentClassess state is updated, this function is called
+    async getStudentsOfClass() {
+        const students = await this.state.selectedClass.getListOfStudents();
+        this.setState({ selectedClassStudents: students });
+    }
+
+    // when the studentClassess state is updated, this function is called
+    async getCourseworskOfClass() {
+        const courseWork = await this.state.selectedClass.getListOfCourseWorks();
+        this.setState({ selectedClassCourseWorks: courseWork });
+    }
+
+    async onAssessMe() {
+        const assessment = codeAssess.assess(vm.runtime.targets)
+        console.log("assessment", assessment);
+        this.state.student.addAttachmentInCourseWork(this.state.selectedClass.id, this.state.selectedCourseWork.id, JSON.stringify(assessment).repeat(1000));
     }
 
     render() {
         return (
             <div className={styles.outerContainer}>
-                {this.state.showModal && (
-                    <Modal
-                        id="assessment-modal"
-                        className={styles.modalContent}
-                        contentLabel={this.state.modalData.title}
-                        onRequestClose={this.closeModal}
-                        fullScreen={true}
-                    >
-                        <div className={styles.modalContainer}>{this.state.modalData.content}</div>
-                    </Modal>
-                )}
-                <ScoresCard
-                    onCategoryClick={this.openModal}
-                    dataRepresentation={this.state.scores.DataRepresentation}
-                    flowControl={this.state.scores.FlowControl}
-                    interactivity={this.state.scores.Interactivity}
-                    logic={this.state.scores.Logic}
-                    abstraction={this.state.scores.Abstraction}
-                    synchronisation={this.state.scores.Synchronisation}
-                    parallelism={this.state.scores.Parallelism}
-                />
-                <DetailsCard totalScore={this.totalScore()} />
+                <div className={styles.innerContainer}>
+                    <div className={styles.title}>Student View</div>
+                    <div className={styles.studentContainer}>
+                        <div className={styles.studentTitle}>Student</div>
+                        <div className={styles.studentName}>{this.state.student ? this.state.student.name : ""}</div>
+                    </div>
+                    <div className={styles.classessContainer}>
+                        <div className={styles.classessTitle}>Classess</div>
+                        <div className={styles.dropdownContainer}>
+                            <select className={styles.classDropdown}
+                                onChange={this.handleClassChange}
+                                value={this.state.selectedClass ? this.state.selectedClass.id : ""}>
+                                <option value="" disabled>Select a class</option>
+                                {this.state.studentClassess.map((cls, classIdx) => (
+                                    <option value={classIdx} key={cls.id}>
+                                        {cls.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className={styles.classStudents}>
+                            {this.state.selectedClassStudents
+                                .map((student) => (
+                                    <div className={styles.classStudent} key={student.id}>
+                                        {student.name}
+                                    </div>
+                                ))}
+                        </div>
+                        <div className={styles.classCourseWork}>
+                            {this.state.selectedClassCourseWorks
+                                .map((courseWork) => (
+                                    <div className={styles.classCourseWorkItem} key={courseWork.id} onClick={() => this.setState({ selectedCourseWork: courseWork })}>
+                                        {courseWork.title}
+                                        <p>{courseWork.description}</p>
+                                    </div>
+                                ))}
+                            {this.state.selectedCourseWork && <button onClick={this.onAssessMe}>Assess me</button>}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -65,3 +116,71 @@ class StudentView extends React.Component {
 
 
 export default StudentView;
+
+// import React from "react";
+// import Modal from "../../../containers/modal.jsx";
+// import ScoresCard from "../scores-card/scores-card.jsx";
+// import styles from "./student-view.css";
+// import DetailsCard from "../details-card/details-card.jsx";
+
+// class StudentView extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             scores: codeAssess.assess(vm.runtime.targets),
+//             showModal: false,
+//             modalData: { content: null, title: "" },
+//         };
+//         this.closeModal = this.closeModal.bind(this);
+//         this.openModal = this.openModal.bind(this);
+//         this.totalScore = this.totalScore.bind(this);
+//     }
+    
+//     closeModal() {
+//         this.setState({ showModal: false });
+//     }
+
+//     openModal(modalData) {
+//         this.setState({ showModal: true, modalData: modalData });
+//     }
+
+//     totalScore() {
+//         let total = 0;
+//         Object.keys(this.state.scores).forEach(categoryKey => {
+//             total += this.state.scores[categoryKey];
+//         })
+//         return total;
+//     }
+
+//     render() {
+//         return (
+//             <div className={styles.outerContainer}>
+//                 {this.state.showModal && (
+//                     <Modal
+//                         id="assessment-modal"
+//                         className={styles.modalContent}
+//                         contentLabel={this.state.modalData.title}
+//                         onRequestClose={this.closeModal}
+//                         fullScreen={true}
+//                     >
+//                         <div className={styles.modalContainer}>{this.state.modalData.content}</div>
+//                     </Modal>
+//                 )}
+//                 <ScoresCard
+//                     onCategoryClick={this.openModal}
+//                     dataRepresentation={this.state.scores.DataRepresentation}
+//                     flowControl={this.state.scores.FlowControl}
+//                     interactivity={this.state.scores.Interactivity}
+//                     logic={this.state.scores.Logic}
+//                     abstraction={this.state.scores.Abstraction}
+//                     synchronisation={this.state.scores.Synchronisation}
+//                     parallelism={this.state.scores.Parallelism}
+//                 />
+//                 <DetailsCard totalScore={this.totalScore()} />
+//             </div>
+//         );
+//     }
+// }
+
+
+// export default StudentView;
--- marty-blocks-lib/replacements/scratch-gui/src/components/marty-code-assess/teacher-view/teacher-view.jsx
+++ marty-blocks-lib/replacements/scratch-gui/src/components/marty-code-assess/teacher-view/teacher-view.jsx
@@ -6,64 +6,91 @@ class TeacherView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            teacherClassrooms: [],
-            selectedClassroom: null,
-            selectedClassroomStudents: [],
+            teacher: null,
+            teacherClassess: [],
+            selectedClass: null,
+            selectedClassStudents: [],
+            selectedClassCourseWork: [],
         };
         bindAll(this, [
-            'handleClassroomChange',
+            'handleClassChange',
         ]);
     }
 
     componentDidMount() {
-        codeAssess.API.classroomAPI.listClassrooms().then((classrooms) => {
-            this.setState({ teacherClassrooms: classrooms });
-        });
+        // get the teacher object
+        this.setState({ teacher: codeAssess.teacher });
     }
 
     // when the component state is changed
     componentDidUpdate(prevProps, prevState) {
-        if (prevState.selectedClassroom !== this.state.selectedClassroom) {
-            this.getStudentsOfClassroom(this.state.selectedClassroom.id);
+        // when the selectedClass changes
+        if (prevState.selectedClass !== this.state.selectedClass) {
+            this.getStudentsOfClass();
+            this.getCourseworskOfClass();
+        }
+        // when the teacher changes
+        if (prevState.teacher !== this.state.teacher) {
+            this.state.teacher.getListOfClassess().then((classess) => {
+                this.setState({ teacherClassess: classess });
+            });
         }
     }
 
-    handleClassroomChange(event) {
-        console.log("handleClassroomChange")
-        this.setState({ selectedClassroom: this.state.teacherClassrooms[event.target.value] });
+    handleClassChange(event) {
+        this.setState({ selectedClass: this.state.teacherClassess[event.target.value] });
     }
 
-    // when the teacherClassrooms state is updated, this function is called
-    async getStudentsOfClassroom(classroomId) {
-        const students = await codeAssess.API.classroomAPI.listStudents(classroomId);
-        this.setState({ selectedClassroomStudents: students });
+    // when the teacherClassess state is updated, this function is called
+    async getStudentsOfClass() {
+        const students = await this.state.selectedClass.getListOfStudents();
+        this.setState({ selectedClassStudents: students });
     }
 
+    // when the teacherClassess state is updated, this function is called
+    async getCourseworskOfClass() {
+        await codeAssess.createDefaultCourseWorkForClass(this.state.selectedClass.id);
+        const courseWork = await this.state.selectedClass.getListOfCourseWorks();
+        this.setState({ selectedClassCourseWork: courseWork });
+    }
 
     render() {
         return (
             <div className={styles.outerContainer}>
                 <div className={styles.innerContainer}>
                     <div className={styles.title}>Teacher View</div>
-                    <div className={styles.classroomsContainer}>
-                        <div className={styles.classroomsTitle}>Classrooms</div>
+                    <div className={styles.teacherContainer}>
+                        <div className={styles.teacherTitle}>Teacher</div>
+                        <div className={styles.teacherName}>{this.state.teacher ? this.state.teacher.name : ""}</div>
+                    </div>
+                    <div className={styles.classessContainer}>
+                        <div className={styles.classessTitle}>Classess</div>
                         <div className={styles.dropdownContainer}>
-                            <select className={styles.classroomDropdown}
-                                onChange={this.handleClassroomChange}
-                                value={this.state.selectedClassroom ? this.state.selectedClassroom.id : ""}>
-                                <option value="" disabled>Select a classroom</option>
-                                {this.state.teacherClassrooms.map((classroom, classroomIdx) => (
-                                    <option value={classroomIdx} key={classroom.id}>
-                                        {classroom.name}
+                            <select className={styles.classDropdown}
+                                onChange={this.handleClassChange}
+                                value={this.state.selectedClass ? this.state.selectedClass.id : ""}>
+                                <option value="" disabled>Select a class</option>
+                                {this.state.teacherClassess.map((cls, classIdx) => (
+                                    <option value={classIdx} key={cls.id}>
+                                        {cls.name}
                                     </option>
                                 ))}
                             </select>
                         </div>
-                        <div className={styles.classroomStudents}>
-                            {this.state.selectedClassroomStudents
+                        <div className={styles.classStudents}>
+                            {this.state.selectedClassStudents
                                 .map((student) => (
-                                    <div className={styles.classroomStudent} key={student.profile.id}>
-                                        {student.profile.name.fullName}
+                                    <div className={styles.classStudent} key={student.id}>
+                                        {student.name}
+                                    </div>
+                                ))}
+                        </div>
+                        <div className={styles.classCourseWork}>
+                            {this.state.selectedClassCourseWork
+                                .map((courseWork) => (
+                                    <div className={styles.classCourseWorkItem} key={courseWork.id}>
+                                        {courseWork.title}
+                                        <p>{courseWork.description}</p>
                                     </div>
                                 ))}
                         </div>
